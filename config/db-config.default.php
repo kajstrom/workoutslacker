bbf7d8fe5330fcf8203685a678d8aa39ebe2d6e3
Kaj Ström <kaj.strom@kapsi.fi>
2016-05-01 21:24:21 +0300

    Fixed incorrect values in default db configs

--- config/db-config.default.php
+++ config/db-config.default.php
@@ -2,3 +2,16 @@
 define("DB_USER", "my user");
 define("DB_PASSWD", "my password");
 
+if (php_sapi_name() != "cli") {
+    if ($_SERVER["SERVER_PORT"] == 8888) {
+        define("DB_NAME", "workout_test");
+    } else {
+        define("DB_NAME", "workout");
+    }
+} else {
+    if (defined("TEST_RUN")) {
+        define("DB_NAME", "workout_test");
+    } else {
+        define("DB_NAME", "workout");
+    }
+}
